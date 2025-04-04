3ef9291a1d8c5f7d3aec96356d776b780e221ff5
kebaz123 <mdrewniak18@gmail.com>
2025-04-04 19:57:47 +0200

    Dodano ulatwienie do korzystania z programu

--- aproksymacja_liczby_PI_konsola/aproksymacja_liczby_PI_konsola/Program.cs
+++ aproksymacja_liczby_PI_konsola/aproksymacja_liczby_PI_konsola/Program.cs
@@ -12,7 +12,29 @@ while (PI_wynik == 0)
         if (n > 0)
         {
             PI_wynik = ClassPI.oblicz_przyblizenie_PI(n);
-            Console.WriteLine(PI_wynik);
+            Console.WriteLine("Przyblizenie liczby PI wynosi "+PI_wynik);
+            Console.WriteLine();
+
+            bool decyzja_o_kontynuacji=true;
+            while (decyzja_o_kontynuacji)
+            {
+                Console.WriteLine("Czy chcesz wylosowac kolejna liczbe? (T/N): ");
+                string wybor_kontynuacja = Console.ReadLine();
+                switch (wybor_kontynuacja.ToUpper())
+                {
+                    case "T":
+                        PI_wynik = 0;
+                        decyzja_o_kontynuacji = false;
+                        break;
+                    case "N":
+                        Console.WriteLine("Dziekuje za skorzystanie z programu");
+                        decyzja_o_kontynuacji = false;
+                        break;
+                    default:
+                        Console.WriteLine("Nieprawidlowy wybor");
+                        break;
+                }
+            }
         }
         else
         {
